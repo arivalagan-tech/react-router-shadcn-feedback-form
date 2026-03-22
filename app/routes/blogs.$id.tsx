d3bcffff9c5519d06debcd996bcf64c522a72c19
arivalagan-tech <arivalagansankar.dev@gmail.com>
2026-03-22 15:23:09 +0530

    updated blog view page styles

--- app/routes/blogs.$id.tsx
+++ app/routes/blogs.$id.tsx
@@ -2,26 +2,25 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { useBlog } from "../context/BlogContext"
 
-export default function BlogDetail() {
+import "../styles/form.css"
 
+export default function BlogDetail() {
   const { id } = useParams()
   const { activeBlog, setActiveBlog } = useBlog()
 
   const [blog, setBlog] = useState<any>(activeBlog)
 
   useEffect(() => {
-
     // ✅ If context empty → fallback API call
     if (!activeBlog && id) {
       fetch(`https://nems-api.roundlogics.com/api/blogs/${id}?populate=*`)
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
           setBlog(data.data)
           setActiveBlog(data.data) // store again in context
         })
-        .catch(err => console.error(err))
+        .catch((err) => console.error(err))
     }
-
   }, [id, activeBlog, setActiveBlog])
 
   // ✅ if blog still empty
@@ -31,19 +30,11 @@ export default function BlogDetail() {
 
   return (
     <div className="blog-detail-container">
+      <h1 className="blog-detail-title">{blog.Cover_Title}</h1>
 
-      <h1 className="blog-detail-title">
-        {blog.Cover_Title}
-      </h1>
+      <p className="blog-detail-author">By {blog.Author_Name}</p>
 
-      <p className="blog-detail-author">
-        By {blog.Author_Name}
-      </p>
-
-      <p className="blog-detail-content">
-        {blog.Content}
-      </p>
-
+      <p className="blog-detail-content">{blog.Content}</p>
     </div>
   )
 }
